1dec56fadddbde8cddf48fc66aba2fe421ae7106
ArekPolus <72617381+ArekPolus@users.noreply.github.com>
2022-01-02 13:48:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+#import my_module
+
+#random_integer = random.randint(1, 10)
+#print(random_integer)
+#print(my_module.pi)
+
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+randomFloat = random.random()*5
+print(randomFloat)
+
+love_score = random.randint(1, 100)
+print(f"your love score is {love_score}")
